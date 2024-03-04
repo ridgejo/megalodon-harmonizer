545d9c9308890ca0ddf9011bea620cdb5475b867
Dulhan Jayalath <dulhan@robots.ox.ac.uk>
2024-03-04 15:02:30 +0000

    Add scaling to multi data loader

--- dataloaders/multi_dataloader.py
+++ dataloaders/multi_dataloader.py
@@ -4,6 +4,8 @@ import sys
 
 from torch.utils.data import ConcatDataset, DataLoader, random_split
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
+from dataloaders.data_utils import BatchScaler
+from dataloaders.armeni2022_labelled import Armeni2022Labelled
 
 class MultiDataLoader(L.LightningDataModule):
     """Loads data from multiple datasets. Supports loading mixed labelled and unlabelled data."""
@@ -12,10 +14,12 @@ class MultiDataLoader(L.LightningDataModule):
         self,
         dataset_preproc_configs : tp.Dict,
         dataloader_configs : tp.Dict,
+        debug : bool = False,
     ):
         super().__init__()
         self.dataset_preproc_configs = dataset_preproc_configs
         self.dataloader_configs = dataloader_configs
+        self.debug = debug
         self.data = {}
 
         self.loaders = {
@@ -29,16 +33,18 @@ class MultiDataLoader(L.LightningDataModule):
 
         for dataset, config in self.dataset_preproc_configs.items():
 
+            print(f"Loading data from {dataset}...")
+
             data, seconds = self.loaders[dataset](config)
-            self.data[dataset] = data
+            self.data.update(data)
 
             print(
-                f"Loaded approximately {seconds // 3600} hours of data from {dataset}."
+                f"Loaded approximately {seconds // 3600} hours of data from {dataset}"
             )
 
     def setup(self, stage: str):
 
-        # Perform train/val/test splits on all datasets
+        print("Performing train/val/test/pred splitting...")
 
         train_ratio, val_ratio, test_ratio, pred_ratio = (
             self.dataloader_configs["train_ratio"],
@@ -65,9 +71,30 @@ class MultiDataLoader(L.LightningDataModule):
             )
 
             self.train[dataset] = DataLoader(train_split, batch_size=batch_size, shuffle=True, drop_last=True)
-            self.val[dataset] = Dataloader(val_split, batch_size=batch_size, shuffle=False, drop_last=False)
-            self.test[dataset]= Dataloader(test_split, batch_size=batch_size, shuffle=False, drop_last=False)
-            self.pred[dataset] = Dataloader(pred_split, batch_size=batch_size, shuffle=True, drop_last=True)
+            self.val[dataset] = DataLoader(val_split, batch_size=batch_size, shuffle=False, drop_last=False)
+            self.test[dataset]= DataLoader(test_split, batch_size=batch_size, shuffle=False, drop_last=False)
+            self.pred[dataset] = DataLoader(pred_split, batch_size=batch_size, shuffle=True, drop_last=True)
+        
+        print("Fitting scalers to datasets...")
+
+        self.scalers = {}
+        norm_conf = self.dataloader_configs["normalisation"]
+        for dataset, train_dl in self.train.items():
+            scaler = BatchScaler(
+                n_sample_batches=norm_conf["n_sample_batches"],
+                per_channel=norm_conf["per_channel"],
+                scaler_conf=norm_conf["scaler_conf"],
+            )
+            scaler.fit(train_dl)
+            self.scalers[dataset] = scaler
+
+    def on_before_batch_transfer(self, batch, dataloader_idx):
+
+        # Apply batch scaling transformation before transferring to device.
+        for dataset, batch_tensor in batch.items():
+            batch[dataset] = self.scalers[dataset](batch_tensor)
+
+        return batch
 
     def train_dataloader(self):
 
@@ -84,12 +111,27 @@ class MultiDataLoader(L.LightningDataModule):
     def predict_dataloader(self):
         return CombinedLoader(self.predict, 'max_size')
 
-    def _load_armeni_2022(self, config):
+    def _load_armeni_2022(self, config, n_subjects=27, n_sessions=10):
 
-        bad_subjects = []
+        # todo: should tag each Dataset with string identifiers: dataset, subject, and session as this is necessary for scalers and any conditional blocks
+        # How to format? dat={}_sub={}_ses={} should work?
+
+        if self.debug:
+            n_subjects = 1
+            n_sessions = 1
+
+        bad_subjects = config["bad_subjects"]
+        bad_sessions = config["bad_sessions"]
+        slice_len = config["slice_len"]
+        label_type = config["label_type"]
+        preproc_config = config["preproc_config"]
+
+        datasets = {}
 
         # Loop over subjects
+        seconds = 0
         for subj_no in range(1, n_subjects + 1):
+
             subject = "{:03d}".format(subj_no)  # 001, 002, and 003
 
             if subject in bad_subjects:
@@ -102,26 +144,58 @@ class MultiDataLoader(L.LightningDataModule):
                 if session in bad_sessions[subject]:
                     continue
 
-                if labels:
-                    data = Armeni2022Labelled(
-                        subject_id=subject,
-                        session=session,
-                        task="compr",
-                        slice_len=slice_len,
-                        preproc_config=preproc_config,
-                        label_type=labels,
-                    )
-                else:
-                    data = Armeni2022(
-                        subject_id=subject,
-                        session=session,
-                        task="compr",  # There is only one relevant task (compr), the emptyroom task is irrelevant.
-                        slice_len=slice_len,
-                        preproc_config=preproc_config,
-                    )
+                # todo: update dataloader
+                data = Armeni2022Labelled(
+                    subject_id=subject,
+                    session=session,
+                    task="compr",
+                    slice_len=slice_len,
+                    preproc_config=preproc_config,
+                    label_type=label_type,
+                )
 
                 seconds += len(data) * slice_len
 
-                datasets.append(data)
+                datasets[f"dat=armeni2022_sub={subject}_ses={session}"] = data
 
         return datasets, seconds
+
+if __name__ == "__main__":
+
+    datamodule = MultiDataLoader(
+        dataset_preproc_configs={
+            "armeni2022": {
+                "bad_subjects": [],
+                "bad_sessions": {"001": [], "002": [], "003": []},
+                "slice_len": 3.0,
+                "label_type": "vad",
+                "preproc_config": {
+                    "filtering": True,
+                    "resample": 250,
+                    "notch_freqs": [50, 100],
+                    "bandpass_lo": 0.5,
+                    "bandpass_hi": 125,
+                },
+            },
+        },
+        dataloader_configs={
+            "train_ratio": 0.9,
+            "val_ratio": 0.04,
+            "test_ratio": 0.04,
+            "pred_ratio": 0.02,
+            "batch_size": 32,
+            "normalisation": {
+                "n_sample_batches": 8,
+                "per_channel": True,
+                "scaler_conf": {
+                    "standard_scaler": None
+                },
+            },
+        },
+        debug=True,
+    )
+
+    datamodule.prepare_data()
+    datamodule.setup(stage="fit")
+
+    breakpoint()
