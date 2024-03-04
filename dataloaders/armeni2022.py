Fix pre-scaling

dataloaders/armeni2022.py
@@ -6,6 +6,7 @@ import typing as tp
 from torch.utils.data import Dataset
 
 import dataloaders.data_utils as data_utils
+import dataloaders.label_utils as label_utils
 
 
 class Armeni2022(Dataset):
@@ -21,6 +22,7 @@ class Armeni2022(Dataset):
         session: str,
         slice_len: float,
         preproc_config: dict,
+        label_type: str,
         bids_root: str = data_utils.DATA_PATH / "armeni2022",
         truncate: tp.Optional[int] = None,
     ):
@@ -32,10 +34,12 @@ class Armeni2022(Dataset):
             task: Recording task ('compr'/'emptyroom').
             slice_len: Length (in seconds) of items returned by getter.
             preproc_config: Dictionary with preprocessing settings.
+            label_type: Required label type (vad / etc.)
         """
 
         self.subject_id = subject_id
         self.session = session
+        self.label_type = label_type
 
         raw, preprocessed, cache_path = data_utils.load_dataset(
             bids_root=bids_root,
@@ -45,8 +49,33 @@ class Armeni2022(Dataset):
             preproc_config=preproc_config,
         )
 
+        events = label_utils.read_events_file(
+            bids_root=bids_root,
+            subject_id=subject_id,
+            session=session,
+            task=task,
+        )
+        
+        textgrid = label_utils.read_textgrid(
+            bids_root=bids_root,
+            subject_id=subject_id,
+            session=session,
+            events=events,
+        )
+
         self.valid_indices, self.samples_per_slice = data_utils.get_valid_indices(raw, slice_len)
         self.num_slices = len(self.valid_indices)
+
+        if label_type == "vad":
+            self.labels = label_utils.get_vad_labels_from_textgrid(textgrid, raw)
+            # self.labels = label_utils.get_vad_labels(events, raw)
+        elif label_type == "voiced":
+            self.phoneme_onsets, self.labels = label_utils.get_voiced_labels(
+                events, raw
+            )
+            self.num_slices = len(self.phoneme_onsets)
+        else:
+            raise ValueError(f"Unrecognised label type {label_type}")
 
         self.raw = raw
 
@@ -57,39 +86,58 @@ class Armeni2022(Dataset):
         return self.num_slices
 
     def __getitem__(self, idx):
-        data_slice, times = data_utils.get_slice(self.raw, idx, self.samples_per_slice, self.valid_indices)
 
-        identifiers = {
-            "dataset": self.__class__.__name__,
-            "subject": self.subject_id,
-            "session": self.session,
-        }
+        if self.label_type is None:
+            data_slice, times = data_utils.get_slice(self.raw, idx, self.samples_per_slice, self.valid_indices)
+            return data_slice, times
 
-        return data_slice, times, identifiers
+        if self.label_type == "vad":
+            data_slice, label_slice, times = label_utils.get_slice(
+                self.raw, self.labels, idx, self.samples_per_slice
+            )
+        elif self.label_type == "voiced":
+            start = self.phoneme_onsets[idx]
+            data_slice, times = self.raw[:, start : start + self.samples_per_slice]
+            label_slice = self.labels[
+                idx
+            ]  # Warning: not actually a slice, just a single label.
+
+        return data_slice, label_slice, times
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     test_dataset = Armeni2022(
-        subject_id="003",
-        session="004",
+        subject_id="001",
+        session="001",
         task="compr",
-        slice_len=5,
+        slice_len=100,#0.3,  # 5
         preproc_config={
             "filtering": True,
-            "resample": 300,
-            "notch_freqs": [50, 100, 150],
-            "bandpass_lo": 0.1,
-            "bandpass_hi": 150,
+            "resample": 250,
+            "notch_freqs": [50, 100],
+            "bandpass_lo": 0.5,
+            "bandpass_hi": 125,
         },
+        label_type="vad",
     )
 
-    data, times, identifiers = test_dataset[7]  # 35-40s
-    for channel in data:
-        plt.plot(times, channel)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Amplitude")
+    # # Plot distribution of voiced labels
+    # all_labels = []
+    # for i in range(1000):
+    #     _, label, _, _, _ = test_dataset[i]
+    #     all_labels.append(label)
+    # plt.plot(all_labels)
+    # plt.savefig("voiced_labels.png")
+    # print(np.histogram(all_labels))
 
-    plt.ylim(-6, 6)
-    plt.savefig("armeni.png")
+    data, labels, times, identifiers = test_dataset[0]  # 35-40s (7)
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+    for channel in data:
+        axes[0].plot(times, channel)
+    axes[1].plot(times, labels)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+
+    plt.savefig("armeni_vad.png")

dataloaders/multi_dataloader.py
@@ -5,7 +5,7 @@ import sys
 from torch.utils.data import ConcatDataset, DataLoader, random_split
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
 from dataloaders.data_utils import BatchScaler
-from dataloaders.armeni2022_labelled import Armeni2022Labelled
+from dataloaders.armeni2022 import Armeni2022
 
 class MultiDataLoader(L.LightningDataModule):
     """Loads data from multiple datasets. Supports loading mixed labelled and unlabelled data."""
@@ -92,7 +92,7 @@ class MultiDataLoader(L.LightningDataModule):
 
         # Apply batch scaling transformation before transferring to device.
         for dataset, batch_tensor in batch.items():
-            batch[dataset] = self.scalers[dataset](batch_tensor)
+            batch[dataset][0] = self.scalers[dataset](batch_tensor[0])
 
         return batch
 
@@ -113,8 +113,7 @@ class MultiDataLoader(L.LightningDataModule):
 
     def _load_armeni_2022(self, config, n_subjects=27, n_sessions=10):
 
-        # todo: should tag each Dataset with string identifiers: dataset, subject, and session as this is necessary for scalers and any conditional blocks
-        # How to format? dat={}_sub={}_ses={} should work?
+        # Dataset key formatted as dat={}_sub={}_ses={}. Necessary for scalers.
 
         if self.debug:
             n_subjects = 1
@@ -144,8 +143,7 @@ class MultiDataLoader(L.LightningDataModule):
                 if session in bad_sessions[subject]:
                     continue
 
-                # todo: update dataloader
-                data = Armeni2022Labelled(
+                data = Armeni2022(
                     subject_id=subject,
                     session=session,
                     task="compr",
@@ -198,4 +196,8 @@ if __name__ == "__main__":
     datamodule.prepare_data()
     datamodule.setup(stage="fit")
 
-    breakpoint()
+    sample = next(iter(datamodule.train_dataloader()))[0]
+    print(sample)
+
+    sample_scaled = datamodule.on_before_batch_transfer(sample, 0)
+    print(sample_scaled)