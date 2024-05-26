3e93c6c6be028a0475184026785151cbda06eff4
Dulhan Jayalath <dhjayalath@protonmail.com>
2024-05-26 22:27:42 +0100

    Add more camcan configs

--- configs/neurips/pnpl/scaling/camcan/random_sample.py
+++ configs/neurips/pnpl/scaling/camcan/random_sample.py
@@ -2,5 +2,5 @@ import random
 from pnpl.datasets.shafto2014 import constants
 
 print("Total subjects", len(constants.SUBJECTS))
-sample = random.sample(constants.SUBJECTS, k=36)
+sample = random.sample(constants.SUBJECTS, k=152)
 print(sample)
