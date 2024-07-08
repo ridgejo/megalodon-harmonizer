a1d049e277af6bdde044369b28af18b91c7c1734
Jeremiah Ridge <wolf6942@htc-login03.arcus-htc.arc.local>
2024-07-08 14:16:17 +0100

    source script

---
+++ scripts/dataset_harmonization/source_megalodon_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Load the required conda script to use mamba
+source /data/engs-pnpl/wolf6942/miniforge3/etc/profile.d/conda.sh
+
+# Get the relative path to the bash directory
+SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+# Activate the mamba environment
+mamba activate megalodon
