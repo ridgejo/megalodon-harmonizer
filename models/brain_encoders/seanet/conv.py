148437345603aa56eb36678fa1f1dcec0d55d706
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-07-10 01:12:25 +0100

    hacky fix

--- models/brain_encoders/seanet/conv.py
+++ models/brain_encoders/seanet/conv.py
@@ -141,13 +141,26 @@ class NormConv1d(nn.Module):
 
     def forward(self, x):
         # x = self.conv(x)
+
+        # # Clone the weights and biases to avoid in-place modifications
+        # weight = self.conv.weight.clone()
+        # weight = weight.to(x.device)
+        # bias = self.conv.bias.clone() if self.conv.bias is not None else None
+        # bias = bias.to(x.device)
         
-        # Clone the weights and biases to avoid in-place modifications
-        weight = self.conv.weight.clone()
-        weight = weight.to(x.device)
-        bias = self.conv.bias.clone() if self.conv.bias is not None else None
-        bias = bias.to(x.device)
-        
+        # Ensure that all parameters of the weight norm are on the same device as the input
+        if self.conv.weight_g.device != x.device:
+            self.conv.weight_g = self.conv.weight_g.to(x.device)
+        if self.conv.weight_v.device != x.device:
+            self.conv.weight_v = self.conv.weight_v.to(x.device)
+        if self.conv.bias is not None and self.conv.bias.device != x.device:
+            self.conv.bias = self.conv.bias.to(x.device)
+            bias = self.conv.bias.clone()
+
+        # Manually compute the weight from weight_g and weight_v
+        weight_norm = torch.norm(self.conv.weight_v, dim=(1, 2), keepdim=True)
+        weight = self.conv.weight_v * (self.conv.weight_g[:, None, None] / weight_norm)
+
         # Perform the convolution manually
         x = F.conv1d(x, weight, bias, stride=self.conv.stride, padding=self.conv.padding,
                       dilation=self.conv.dilation, groups=self.conv.groups)
@@ -176,11 +189,24 @@ class NormConv2d(nn.Module):
     def forward(self, x):
         # x = self.conv(x)
 
-        # Clone the weights and biases to avoid in-place modifications
-        weight = self.conv.weight.clone()
-        weight = weight.to(x.device)
-        bias = self.conv.bias.clone() if self.conv.bias is not None else None
-        bias = bias.to(x.device)
+        # # Clone the weights and biases to avoid in-place modifications
+        # weight = self.conv.weight.clone()
+        # weight = weight.to(x.device)
+        # bias = self.conv.bias.clone() if self.conv.bias is not None else None
+        # bias = bias.to(x.device)
+
+        # Ensure that all parameters of the weight norm are on the same device as the input
+        if self.conv.weight_g.device != x.device:
+            self.conv.weight_g = self.conv.weight_g.to(x.device)
+        if self.conv.weight_v.device != x.device:
+            self.conv.weight_v = self.conv.weight_v.to(x.device)
+        if self.conv.bias is not None and self.conv.bias.device != x.device:
+            self.conv.bias = self.conv.bias.to(x.device)
+            bias = self.conv.bias.clone()
+
+        # Manually compute the weight from weight_g and weight_v
+        weight_norm = torch.norm(self.conv.weight_v, dim=(1, 2), keepdim=True)
+        weight = self.conv.weight_v * (self.conv.weight_g[:, None, None] / weight_norm)
         
         # Perform the convolution manually
         x = F.conv2d(x, weight, bias, stride=self.conv.stride, padding=self.conv.padding,
