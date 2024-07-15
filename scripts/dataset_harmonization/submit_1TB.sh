5957628a838f782b2211bdbb23b1c7893a656cd5
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-07-15 17:22:53 +0100

    update max time

--- scripts/dataset_harmonization/submit_1TB.sh
+++ scripts/dataset_harmonization/submit_1TB.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=system
 #SBATCH --clusters=htc
 #SBATCH --job-name=ptMEGall
-#SBATCH --time=11:59:00
+#SBATCH --time=175:00:00
 #SBATCH --gres=gpu:1 --constraint='gpu_sku:A100'
 #SBATCH --partition=test
 #SBATCH --output=slurm_log/slurm-%j.out
