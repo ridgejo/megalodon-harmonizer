c893fb65480adef7347c15762e060504a2bba705
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-07-22 16:29:44 +0100

    scaling

--- models/confusion_loss.py
+++ models/confusion_loss.py
@@ -8,14 +8,31 @@ import numpy as np
 ########################################################################################################################
 
 class ConfusionLoss(nn.Module):
-    def __init__(self, task=0):
+    def __init__(self, task=0, epsilon=1e-6):
         super(ConfusionLoss, self).__init__()
         self.task = task
+        self.epsilon = epsilon
 
     def forward(self, x, target):
+        # Check for NaNs or Infs in input
+        if torch.isnan(x).any():
+            raise ValueError("NaN detected in input to ConfusionLoss")
+        if torch.isinf(x).any():
+            raise ValueError("Inf detected in input to ConfusionLoss")
+
+        # Add epsilon to x to avoid log(0)
+        x = x + self.epsilon
+
         # We only care about x
         log = torch.log(x)
         log_sum = torch.sum(log, dim=1)
         normalised_log_sum = torch.div(log_sum,  x.size()[1])
         loss = torch.mul(torch.sum(normalised_log_sum, dim=0), -1)
+
+        # Check for NaNs or Infs in output
+        if torch.isnan(loss).any():
+            raise ValueError("NaN detected in input to ConfusionLoss")
+        if torch.isinf(loss).any():
+            raise ValueError("Inf detected in input to ConfusionLoss")
+
         return loss
