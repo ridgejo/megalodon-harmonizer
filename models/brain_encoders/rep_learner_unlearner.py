a1532deda8f72e3ff888c9e9b74a1054cad3db98
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-08-01 11:36:36 +0100

    testing

--- models/brain_encoders/rep_learner_unlearner.py
+++ models/brain_encoders/rep_learner_unlearner.py
@@ -969,14 +969,15 @@ class RepLearnerUnlearner(L.LightningModule):
 
     def on_val_epoch_end(self):
         if self.current_epoch == self.epoch_stage_1 - 1:
-            # for param_group in self.trainer.optimizers["step1_optim"]:
-            #     param_group["lr"] = self.learning_rate
-            # for param_group in self.trainer.optimizers["optim"]:
-            #     param_group["lr"] = self.task_learning_rate
-            # for param_group in self.trainer.optimizers["conf_optim"]:
-            #     param_group["lr"] = self.conf_learning_rate
-            # for param_group in self.trainer.optimizers["dm_optim"]:
-            #     param_group["lr"] = self.dm_learning_rate
+            for param_group in self.trainer.optimizers["step1_optim"]:
+                param_group["lr"] = self.learning_rate
+            for param_group in self.trainer.optimizers["optim"]:
+                param_group["lr"] = self.task_learning_rate
+            for param_group in self.trainer.optimizers["conf_optim"]:
+                param_group["lr"] = self.conf_learning_rate
+            for param_group in self.trainer.optimizers["dm_optim"]:
+                param_group["lr"] = self.dm_learning_rate
+                
             for optimizer in self.trainer.optimizers:
                 for param_group in optimizer:
                     for param in param_group["params"]:
