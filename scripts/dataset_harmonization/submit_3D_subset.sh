966624f57a5dd934e26e220969073cdba18cc935
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-08-06 00:05:31 +0100

    3D submit

---
+++ scripts/dataset_harmonization/submit_3D_subset.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=jeremiah.ridge@wolfson.ox.ac.uk
+#SBATCH --nodes=1
+#SBATCH --mem=500G
+#SBATCH --qos=system
+#SBATCH --clusters=htc
+#SBATCH --job-name=subset_test
+#SBATCH --time=23:59:00
+#SBATCH --gres=gpu:1 --constraint='gpu_mem:32GB'
+#SBATCH --partition=long
+#SBATCH --output=slurm_log/slurm-%j.out
+
+# source env
+source scripts/dataset_harmonization/source_megalodon_env.sh
+
+# run exp
+srun python $@
+
