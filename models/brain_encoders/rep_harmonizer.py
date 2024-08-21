4605a357035e7e064f59dbd2aafa22752b95fb75
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-08-21 13:13:35 +0100

    debug

--- models/brain_encoders/rep_harmonizer.py
+++ models/brain_encoders/rep_harmonizer.py
@@ -655,11 +655,17 @@ class RepHarmonizer(L.LightningModule):
                 if self.sdat:
                     optim.first_step(zero_grad=True)
                 else:
-                    print(f"Version of film linear weight before step: {self.encoder_models["subject_film_module"].lin.weight._version}")
-                    print(f"Version of seannet conv1 weight before step: {self.encoder_models["encoder"].model[0].conv.conv.weight._version}")
+                    # print(f"Version of film linear weight before step: {self.encoder_models["subject_film_module"].lin.weight._version}")
+                    # print(f"Version of seannet conv1 weight before step: {self.encoder_models["encoder"].model[0].conv.conv.weight._version}")
+                    pre_step_v = []
+                    for param in list(filter(lambda p: p.requires_grad, self.encoder_models.parameters())):
+                        pre_step_v.append((param, param._version))
                     optim.step()
-                    print(f"Version of film linear weight after step: {self.encoder_models["subject_film_module"].lin.weight._version}")
-                    print(f"Version of seannet conv1 weight after step: {self.encoder_models["encoder"].model[0].conv.conv.weight._version}")
+                    for idx, param in enumerate(list(filter(lambda p: p.requires_grad, self.encoder_models.parameters()))):
+                        if param._version != pre_step_v[idx][1]:
+                            print(f"Version of {param} before step is {pre_step_v[idx][1]} and after is {param._version}", flush=True)
+                    # print(f"Version of film linear weight after step: {self.encoder_models["subject_film_module"].lin.weight._version}")
+                    # print(f"Version of seannet conv1 weight after step: {self.encoder_models["encoder"].model[0].conv.conv.weight._version}")
 
 
                 # update just domain classifier
