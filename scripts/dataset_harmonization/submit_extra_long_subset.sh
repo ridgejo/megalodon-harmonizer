57f2e776d9b7d8e8c3b82501493039f23ad63004
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-08-26 21:14:41 +0100

    extra lond submit

---
+++ scripts/dataset_harmonization/submit_extra_long_subset.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=jeremiah.ridge@wolfson.ox.ac.uk
+#SBATCH --nodes=1
+#SBATCH --mem=300G
+#SBATCH --qos=system
+#SBATCH --clusters=htc
+#SBATCH --job-name=subset_test
+#SBATCH --time=49:59:00
+#SBATCH --gres=gpu:1
+#SBATCH --partition=long
+#SBATCH --output=slurm_log/slurm-%j.out
+
+# source env
+source scripts/dataset_harmonization/source_megalodon_env.sh
+
+# run exp
+srun python $@
+
