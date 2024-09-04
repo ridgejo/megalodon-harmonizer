cd7d65c733ec25708a121ce939807e6f74f79c25
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-09-04 16:11:29 +0100

    arc submit

---
+++ scripts/dataset_harmonization/submit_long_arc.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=jeremiah.ridge@wolfson.ox.ac.uk
+#SBATCH --nodes=1
+#SBATCH --mem=300G
+#SBATCH --qos=system
+#SBATCH --clusters=arc
+#SBATCH --job-name=subset_test
+#SBATCH --time=29:59:00
+#SBATCH --partition=long
+#SBATCH --output=slurm_log/slurm-%j.out
+
+# source env
+source scripts/dataset_harmonization/source_megalodon_env.sh
+
+# run exp
+srun python $@
+
