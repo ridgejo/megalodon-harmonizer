5391fd2892e1e9b95a1204b57f568a0bb74fcc34
Jeremy Ridge <jeremy@Jeremys-MacBook-Pro.local>
2024-09-06 19:29:47 +0100

    testing tsne fixes

---
+++ scripts/dataset_harmonization/submit_get_tsne_arc.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=jeremiah.ridge@wolfson.ox.ac.uk
+#SBATCH --nodes=1
+#SBATCH --mem=300G
+#SBATCH --qos=system
+#SBATCH --clusters=arc
+#SBATCH --job-name=getTSNE
+#SBATCH --time=11:59:00
+#SBATCH --partition=short
+#SBATCH --output=slurm_log/slurm-%j.out
+
+# source env
+source scripts/dataset_harmonization/source_megalodon_env.sh
+
+# run exp
+srun python $@
+
